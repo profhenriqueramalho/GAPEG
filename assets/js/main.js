36ada5ac9b5cb1d49b463431ded682051abde153
profhenriqueramalho <henriqueramalho14@gmail.com>
2026-02-05 10:12:38 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+(() => {
+  const path = (location.pathname.split("/").pop() || "index.html").toLowerCase();
+  document.querySelectorAll(".menu a").forEach(a => {
+    const href = (a.getAttribute("href") || "").toLowerCase();
+    if (href === path) a.classList.add("active");
+  });
+})();
